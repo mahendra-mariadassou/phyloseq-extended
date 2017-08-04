2abbfcb22de6942799a9a54a9e15d65d3d6bf5dc
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2017-08-04 11:10:37 +0200

    Correct import of Chaillou data in tests.

--- test/test.R
+++ test/test.R
@@ -3,6 +3,7 @@ library(phyloseq)
 library(reshape2)
 library(ape)
 library(gridExtra)
+library(plotly)
 
 source("graphical_methods.R")
 source("tree_methods.R")
@@ -18,10 +19,27 @@ source("compute_niche.R")
 
 ## Import Chaillou data
 food <- import_biom(BIOMfilename = "test/Chaillou/chaillou.biom", 
-                    treefilename = "test/Chaillou/tree.nwk")
+                    treefilename = "test/Chaillou/tree.nwk",
+                    parseFunction = parse_taxonomy_greengenes)
+
 sample_data(food)$EnvType <- factor(sample_data(food)$EnvType, 
                                     levels = c("DesLardons", "MerguezVolaille", "BoeufHache", "VeauHache", 
                                                "SaumonFume", "FiletSaumon", "FiletCabillaud", "Crevette"))
+
+## Change behavior of plot composition
+p <- plot_composition(food, "Kingdom", "Bacteria", "Phylum", fill = "Phylum", facet_grid = "~EnvType")
+ggplotly(p, tooltip = c("fill", "y", "x"))
+
+## Change behavior of plot ordination
+dist.uf <- distance(food, method = "unifrac")
+p <- plot_ordination(food, ordination = ordinate(food, distance = dist.uf, method = "MDS"), 
+                     color = "EnvType")
+plot(p)
+ggplotly(p + geom_text(aes(label = EnvType), color = "transparent"), tooltip = c("label"))
+
+
+
+## Test plot_composition
 
 ## test different gradient for plot_dist_as_heatmap
 dist.uf <- distance(food, "unifrac")
