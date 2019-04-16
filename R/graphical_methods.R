b19f0d4f3fa18dd5bc7380979ef43118209202fc
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2019-04-16 10:16:31 +0200

    avoid conflicts with listenv

--- R/graphical_methods.R
+++ R/graphical_methods.R
@@ -609,7 +609,7 @@ plot_clust <- function(physeq, dist, method = "ward.D2", color = NULL,
   clust <- as.phylo(hclust(dist, method = method))
   ## change tip label if needed
   if (!is.null(label)) {
-    tip.dict <- setNames(as.character(get_variable(physeq, label)),
+    tip.dict <- setNames(as.character(phyloseq::get_variable(physeq, label)),
                          sample_names(physeq))
     clust$tip.label <- tip.dict[clust$tip.label]
   }
