e2c1d7399e64ebe1a953bc9399040a18759e65f8
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2019-05-30 14:06:29 +0200

    Comment everything in load-extra-functions

--- R/load-extra-functions.R
+++ R/load-extra-functions.R
@@ -1,24 +1,24 @@
 ## Load packages
-library(ggplot2)
-library(phyloseq)
-library(reshape2)
-library(ape)
-library(gridExtra)
+# library(ggplot2)
+# library(phyloseq)
+# library(reshape2)
+# library(ape)
+# library(gridExtra)
 
-scripts <- c("graphical_methods.R",
-             "tree_methods.R",
-             "plot_merged_trees.R",
-             "specificity_methods.R",
-             "ternary_plot.R",
-             "richness.R",
-             "edgePCA.R",
-             "copy_number_correction.R",
-             "import_frogs.R",
-             "prevalence.R",
-             "compute_niche.R")
-urls <- paste0("https://raw.githubusercontent.com/mahendra-mariadassou/phyloseq-extended/master/R/", scripts)
-
-for (url in urls) {
-  # source(url)
-}
+# scripts <- c("graphical_methods.R",
+#              "tree_methods.R",
+#              "plot_merged_trees.R",
+#              "specificity_methods.R",
+#              "ternary_plot.R",
+#              "richness.R",
+#              "edgePCA.R",
+#              "copy_number_correction.R",
+#              "import_frogs.R",
+#              "prevalence.R",
+#              "compute_niche.R")
+# urls <- paste0("https://raw.githubusercontent.com/mahendra-mariadassou/phyloseq-extended/master/R/", scripts)
+#
+# for (url in urls) {
+#   # source(url)
+# }
 
