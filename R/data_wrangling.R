41b589e6e39336df11b82669b0465508dc20ae9e
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2019-07-23 17:45:25 +0200

    Update doc for sample_groups

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -1,19 +1,20 @@
 #' @title Merge samples based on a grouping variable given either as a sample variable or a factor.
 #'
-#' @description The purpose of this method is to merge/agglomerate the sample indices of a phyloseq object according to a categorical variable contained in a sample_data or a provided factor. Unlike the original \code{\link{phyloseq::merge_samples}}, sample metada are not coerced to factors. Instead, the metadata from the first sample in each group is kept.
+#' @description The purpose of this method is to merge/agglomerate the sample indices of a phyloseq object according to a categorical variable contained in a sample_data or a provided factor. Unlike the original \code{\link{merge_samples}}, sample metada are not coerced to factors. Instead, the metadata from the first sample in each group is kept.
 #'
 #' @inheritParams phyloseq::merge_samples
 #'
 #' @return A phyloseq object that has had its sample indices merged according to the factor indicated by the group argument. The output class matches x.
 #'
-#' @seealso \code{\link{phyloseq::merge_samples}}
+#' @seealso \code{\link{merge_samples}}
 #'
 #' @export
 #'
 #' @examples
 #' data(food)
 #' merge_group(food, "EnvType")
-#'
+#' ## Note that sample data are preserved
+#' sample_data(food)
 merge_group <- function(physeq, group, fun = mean) {
   # Build grouping factor
   if (!is.null(sample_data(physeq, FALSE))) {
