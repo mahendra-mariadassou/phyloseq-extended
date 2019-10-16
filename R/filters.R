be62a9b1667bbc7fc728392d15e611d6015e5233
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2019-10-16 16:18:11 +0200

    update doc

--- R/filters.R
+++ R/filters.R
@@ -4,22 +4,27 @@ filter_abundance <- function(physeq, frac = 0.001, A = 0.05 * nsamples(physeq)) 
   return(prune_taxa(tokeep, physeq))
 }
 
-
-## filter OTU with prevalence higher than 50% in each level of factor group
-## and highest relative abundance higher than 0.1%
-
-
 #' Prevalence and abundance filters, applied independently on each group
 #'
-#' @param physeq A \code{phyloseq} class object
-#' @param prev.thresh Minimum prevalence in a group for a taxa to be kept
-#' @param abund.thresh Minimum relative abundance in a group for a taxa to be kept
-#' @param group Groups
-#' @param rarefy Logical. Should samples be rarefied before abundances and prevalence are computed.
+#' @param physeq A \link{phyloseq} class object
+#' @param prev.thresh Minimum prevalence in at least one group level for a taxa to be kept
+#' @param abund.thresh Minimum relative abundance in at least one sample
+#' @param group Either the a single character string matching a variable name in
+#'              the corresponding sample_data of `physeq`, or a factor with the same
+#'              length as the number of samples in `physeq`.
+#' @param rarefy Logical. Should samples be rarefied before abundances and prevalence are computed?
 #'
-#' @return A list of taxa that pass the fitlers. To be used with \code{prune_sample}
+#' @details Apply prevalence and abundance based filters to a \link{phyloseq} class object. The relative abundance threshold needs to be satisfied in at least one sample (i.e. abundances are not computed per group). The prevalence needs to be satisfied in at least one group level (or in the global dataset if no group is provided). The samples are rarefied by default to avoid depth-induced biases on discovery rates for the prevalence threshold.
+#'
+#' @return Vector of taxa that pass the filters, for further use with \link{prune_sample}.
+#'
 #' @export
 #'
+#' @examples
+#' data(food) ## 507 taxa
+#' ## Taxa with prevalence > 25% in any food type and abundance > 1% in at least one sample.
+#' taxa_to_keep <- filter_phyloseq(food, 0.25, 0.01, group = "EnvType")
+#' prune_taxa(taxa_to_keep, food) ## 217 taxa
 filter_phyloseq <- function(physeq, prev.thresh = 0.5, abund.thresh = 0.001, group = rep(1, nsamples(physeq)), rarefy = TRUE) {
   physeq <- suppressMessages(physeq %>% rarefy_even_depth(trimOTUs = FALSE, rngseed = 20171201))
   ## Abundant otus
