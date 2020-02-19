af8d92b34a31ec06d5cec593a5b8ded9ecf9e2bb
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2020-02-19 18:00:07 +0100

    estimate_prevalence now returns local prevalence.

--- R/prevalence.R
+++ R/prevalence.R
@@ -1,12 +1,12 @@
-#' Compute local prevalence and specificity of an OTU in different levels of a group
+#' Compute local prevalence, abundance and specificity of an OTU in different levels of a group
 #'
 #' @param physeq phyloseq class object, otu abundances are extracted from this object
 #' @param group Either the a single character string matching a variable name in
 #'              the corresponding sample_data of `physeq`, or a factor with the same
 #'              length as the number of samples in `physeq`.
 #' @param format long (default) or wide. Should the result be organised as a long data.frame
-#'               with columns "otu", "group", "prevalence" and "specificity" or a numeric matrix
-#'               with otus in rows and group specificty/group prevalence in column.
+#'               with columns "otu", "group", "prevalence", "specificity" and "abundance" or a numeric matrix
+#'               with otus in rows and group specificty/group prevalence/group abundance in column.
 #' @param rarefy (Logical). Default TRUE. Rarefy samples before computing prevalences.
 #'
 #' @details Specificity is defined here as local prevalence / global prevalence,
@@ -16,12 +16,14 @@
 #' @return A tibble with components
 #' * prevalence: observed prevalence in group
 #' * specifity : specifity (prevalence / global prevalence) in group
+#' * abundance : relative abundance in group
 #' * group: factor level
 #' * otu: otu
-#' or a matrix m of size `ntaxa(physeq)` times 2*N where
+#' or a matrix m of size `ntaxa(physeq)` times 3*N where
 #' N is the number of levels (groups) in `group` and
 #' * `m[i, g]` is the prevalence of otu i in group g
 #' * `m[i, N+g]` is the specificity of otu i to group g
+#' * `m[i, 2N+g]` is the relative abundance of otu i in group g
 #' @export
 #'
 #' @importFrom phyloseq sample_data otu_table rarefy_even_depth
@@ -51,6 +53,10 @@ estimate_prevalence <- function(physeq, group, format = c("long", "wide"), raref
     ## Construct relative abundances by sample
     tdf <- as(phyloseq::otu_table(physeq), "matrix")
     if (!taxa_are_rows(physeq)) { tdf <- t(tdf) }
+    ## abundance
+    tdf[, ] <- apply(tdf, 2, function(x) { x / sum(x) })
+    abund <- t(rowsum(t(tdf), group, reorder = TRUE)) / matrix(rep(table(group), each = nrow(tdf)),
+                                                               nrow = nrow(tdf))
     tdf <- 0 + (tdf > 0)
     ## prevalence
     frac <- t(rowsum(t(tdf), group, reorder = TRUE)) / matrix(rep(table(group), each = nrow(tdf)),
@@ -63,13 +69,14 @@ estimate_prevalence <- function(physeq, group, format = c("long", "wide"), raref
         ## Pad colnames
         colnames(frac) <- paste("prev", colnames(frac), sep = "_")
         colnames(spec) <- paste("spec", colnames(spec), sep = "_")
-        ## cbind prevalence and specificity
-        return(cbind(frac, spec))
+        colnames(abund) <- paste("abund", colnames(abund), sep = "_")
+        ## cbind prevalence, specificity and abundances
+        return(cbind(frac, spec, abund))
     }
     ## Melt and join tables
-    dplyr::inner_join(dplyr::as_tibble(frac, rownames = "otu") %>%
-                          tidyr::gather(key = "group", value = "prevalence", -otu),
-                      dplyr::as_tibble(spec, rownames = "otu") %>%
-                          tidyr::gather(key = "group", value = "specificity", -otu),
-                      by = c("otu", "group"))
+    dplyr::as_tibble(frac, rownames = "otu") %>% tidyr::gather(key = "group", value = "prevalence", -otu) %>%
+        dplyr::inner_join(dplyr::as_tibble(spec, rownames = "otu") %>% tidyr::gather(key = "group", value = "specificity", -otu),
+                          by = c("otu", "group")) %>%
+        dplyr::inner_join(dplyr::as_tibble(abund, rownames = "otu") %>% tidyr::gather(key = "group", value = "abundance", -otu),
+                          by = c("otu", "group"))
 }
